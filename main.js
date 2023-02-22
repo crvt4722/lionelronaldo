12b9db41c5b5d8945e11c82a7d1b696af99e3f96
crvt4722 <phamvantoi2002@gmail.com>
2023-02-22 16:26:56 +0700

    Create a new responsibility

---
+++ main.js
@@ -0,0 +1,12 @@
+let headerWrapperIcon = document.querySelector('.header-wrapper__icon')
+
+setInterval(function(){
+    headerWrapperIcon.style.display = 'flex'
+}, 350)
+
+setInterval(function(){
+    headerWrapperIcon.style.display = 'none'
+}, 700)
+
+let x = prompt('toiasas')
+console.log(xsaa)
