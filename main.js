9563ee65432aa6106f2aada7031044b6daefc490
Pham Van Toi <phamvantoi2002@gmail.com>
2023-02-24 22:49:32 +0700

    Add slider

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+'use strict'
+
+// Header
+
 let headerWrapperIcon = document.querySelector('.header-wrapper__icon')
 
 setInterval(function(){
@@ -8,5 +12,31 @@ setInterval(function(){
     headerWrapperIcon.style.display = 'none'
 }, 700)
 
-let x = prompt('toiasas')
-console.log(xsaa)
+
+// Slider
+let sliderImg = document.getElementById('slider__img')
+let controlPrev = document.querySelector('.prev')
+let controlNext = document.querySelector('.next')
+
+var cnt = 1
+
+// The function to be controlled the slider.
+function controlSlider(option){
+    if(option == 'next'){
+        cnt = (cnt + 1)%4
+        if (cnt == 0) cnt = 1
+    }
+    else {
+        cnt = (cnt - 1)%4
+        if (cnt == 0) cnt = 3
+    }
+    let path = './assets/img/rosi' +cnt +'.jpg'
+    sliderImg.src = path
+}
+
+controlNext.addEventListener('click', controlSlider.bind(this,'next'))
+controlPrev.addEventListener('click', controlSlider.bind(this,'prev'))
+
+// Auto run slider.
+setInterval(controlSlider.bind(this, 'next'), 2500)
+
